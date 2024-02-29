aa5e330c91576c8a32fb8cdd2e609b637649d277
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-02-29 13:07:46 +0200

    Added webex and eclass links

--- src/CustomCard/CustomCard.jsx
+++ src/CustomCard/CustomCard.jsx
@@ -1,46 +1,58 @@
 import "./CustomCard.css";
 import Button from "@mui/material/Button";
+import StarIcon from '@mui/icons-material/Star';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 
-const CustomCard = ({ title, schedule, link, semester }) => {
-    const semesterColors = {
-        2: '#59D5E0',
-        4: '#F5DD61',
-        6: '#FAA300',
-        8: '#F4538A'
-    };
+const CustomCard = ({ title, schedule, webex, eclass, semester, isFavorite, toggleFavorite }) => {
+  const semesterColors = {
+    2: '#59D5E0',
+    4: '#F5DD61',
+    6: '#FAA300',
+    8: '#F4538A'
+  };
 
-    const headerColor = semesterColors[semester];
+  const headerColor = semesterColors[semester];
 
-    const headerStyle = {
-        backgroundColor: headerColor,
-        padding: '16px',
-        textAlign: 'center'
-    };
+  const headerStyle = {
+    backgroundColor: headerColor,
+    padding: '16px',
+    textAlign: 'center',
+  };
 
-    const scheduleItems = schedule.split(', ');
 
-    return (
-        <div className="card" style={{ flex: '1', margin: '0.5%', minWidth: '300px', display: 'flex', flexDirection: 'column' }}>
-            <div className="header" style={headerStyle}>
-                <h1>{title}</h1>
-            </div>
-            <div className="content" style={{ flex: '1', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                <h6 style={{fontWeight:"bold"}}>Εξάμηνο: {semester}ο</h6>
-                <div className="schedule-section">
-                    <h3 style={{ textDecoration: "underline", textAlign: "center" }}>Πρόγραμμα:</h3>
-                    <ul style={{ listStyle: "none", padding: "0", textAlign: "center" }}>
-                        {scheduleItems.map((item, index) => (
-                            <li key={index}>{item}</li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="button-section" style={{ textAlign: 'center', marginBottom: '16px' }}>
-                    <Button variant="contained" color="primary" onClick={() => window.location.href = 'YOUR_ECLASS_LINK_HERE'}>eClass</Button>
-                    <Button variant="contained" color="success" onClick={() => window.open(link, "_blank")}>Webex</Button>
-                </div>
-            </div>
+  const scheduleItems = schedule.split(', ');
+
+  const handleFavoriteToggle = () => {
+    toggleFavorite(title);
+  };
+
+  
+
+  return (
+    <div className="card" style={{ flex: '1', margin: '0.5%', minWidth: '300px', display: 'flex', flexDirection: 'column' }}>
+      <div className="header" style={headerStyle}>
+        <h1>{title}</h1>
+      </div>
+      <div className="content" style={{ flex: '1', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+        <h6 style={{ fontWeight: "bold" }}>Εξάμηνο: {semester}ο</h6>
+        <div className="schedule-section">
+          <h3 style={{ textDecoration: "underline", textAlign: "center" }}>Πρόγραμμα:</h3>
+          <ul style={{ listStyle: "none", padding: "0", textAlign: "center" }}>
+            {scheduleItems.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
         </div>
-    );
+        <div className="button-section" style={{ textAlign: 'center', marginBottom: '16px' }}>
+          <Button variant="contained" color="primary" href={eclass}>eClass</Button>
+          <Button variant="contained" color="success" href={webex}>Webex</Button>
+          <Button variant="contained" color={isFavorite ? "secondary" : "primary"} onClick={handleFavoriteToggle}>
+            {isFavorite ? 'Remove from favorites' : 'Add to favorites'}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CustomCard;
