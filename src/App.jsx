bbe219f9bb31b90bdcc10f4222afc49ea60b991a
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-02-29 16:50:00 +0200

    Added cookie to never expire and cookie reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import './App.css';
 import CustomCard from './CustomCard/CustomCard';
 import Footer from './footer';
@@ -6,55 +7,80 @@ import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
 import courseData from './courseData.json';
+import Button from '@mui/material/Button';
 
 const App = () => {
-  // Initialize courses with an additional isFavorite property set to false
-  const initialCourses = courseData.map(course => ({ ...course, isFavorite: false }));
-  const [courses, setCourses] = useState(initialCourses);
+  const [courses, setCourses] = useState([]);
   const [searchInput, setSearchInput] = useState('');
+
+  useEffect(() => {
+    const storedFavorites = Cookies.get('favoriteCourses');
+    const favoriteCourses = storedFavorites ? JSON.parse(storedFavorites) : [];
+
+    const initializedCourses = courseData.map(course => ({
+      ...course,
+      isFavorite: favoriteCourses.includes(course.key),
+    }));
+
+    setCourses(initializedCourses);
+  }, []);
 
   const toggleFavorite = (courseKey) => {
     const updatedCourses = courses.map(course => {
       if (course.key === courseKey) {
-        // Toggle the isFavorite property
         return { ...course, isFavorite: !course.isFavorite };
       }
       return course;
     });
+
     setCourses(updatedCourses);
+
+    const favoriteCoursesKeys = updatedCourses.filter(course => course.isFavorite).map(course => course.key);
+    Cookies.set('favoriteCourses', JSON.stringify(favoriteCoursesKeys), { expires: 3650 });
   };
 
   const handleSearchInputChange = (event) => {
     setSearchInput(event.target.value);
   };
 
-  // Filter courses based on search input and sort by favorites
-  const filteredAndSortedCourses = courses
-    .filter(course => 
-      course.title.toLowerCase().includes(searchInput.toLowerCase()))
-    .sort((a, b) => b.isFavorite - a.isFavorite);
+  const clearFavorites = () => {
+    Cookies.remove('favoriteCourses');
+    const resetCourses = courses.map(course => ({ ...course, isFavorite: false }));
+    setCourses(resetCourses);
+  };
+
+  const filteredAndSortedCourses = courses.filter(course => 
+    course.title.toLowerCase().includes(searchInput.toLowerCase())
+  ).sort((a, b) => b.isFavorite - a.isFavorite);
 
   return (
     <>
       <div className='app'>
-        <h1>DIT Webex Links</h1>
-        <h3>Εαρινό Εξάμηνο 2024</h3>
-        <hr />
+        
+          <h1>DIT Webex Links</h1>
+          <h3>Εαρινό Εξάμηνο 2024</h3>
+          <hr />
+          <TextField
+            label="Αναζήτηση"
+            variant="outlined"
+            value={searchInput}
+            onChange={handleSearchInputChange}
+            sx={{width:'30%', margin:'0 3%'}}
+            size='small'
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
 
-        <TextField
-          label="Αναζήτηση"
-          value={searchInput}
-          onChange={handleSearchInputChange}
-          size="small"
-          sx={{width:'30%', margin:'0 3%'}}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <SearchIcon />
-              </InputAdornment>
-            )
-          }}
-        />
+          />
+          <br/>
+          <Button variant="contained" color="secondary" onClick={clearFavorites} style={{textTransform:'none'}}>
+            Καθαρισμός Αγαπημένων
+          </Button>
+  
 
         <div className='main'>
           {filteredAndSortedCourses.map(course => (
@@ -70,6 +96,7 @@ const App = () => {
             />
           ))}
         </div>
+
         <Footer />
       </div>
     </>
