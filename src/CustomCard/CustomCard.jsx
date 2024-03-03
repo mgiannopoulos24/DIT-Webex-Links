a4b9438ffd3ffe79d0c0c4b81a3f89e70088e0c6
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-03-03 13:51:04 +0200

    Needs fixing. Added live indicator

--- src/CustomCard/CustomCard.jsx
+++ src/CustomCard/CustomCard.jsx
@@ -4,8 +4,13 @@ import StarIcon from '@mui/icons-material/StarRounded';
 import StarBorderIcon from '@mui/icons-material/StarBorderRounded';
 import VideocamRoundedIcon from '@mui/icons-material/VideocamRounded';
 import SchoolRoundedIcon from '@mui/icons-material/SchoolRounded';
+import { Circle } from "@mui/icons-material";
+import courseData from '../courseData.json';
 
 const CustomCard = ({ title, schedule, webex, eclass, semester, isFavorite, toggleFavorite,darkMode }) => {
+
+  console.log(schedule);
+
   const semesterColors = {
     2: '#add8e6',
     4: '#F5DD61',
@@ -22,12 +27,29 @@ const CustomCard = ({ title, schedule, webex, eclass, semester, isFavorite, togg
   };
 
 
-  const scheduleItems = schedule.split(', ');
+  const scheduleItems = typeof schedule === 'string' ? schedule.split(', ') : [];
 
   const handleFavoriteToggle = () => {
     toggleFavorite(title);
   };
 
+  function isLiveNow(dateTime, startDate, endDate) {
+    const time = dateTime.getTime();
+    return time >= startDate.getTime() && time <= endDate.getTime();
+  }
+
+  // const now = new Date();
+  const now = new Date();
+  const startTime = new Date(courseData.scheduleStart); 
+  const endTime = new Date(courseData.scheduleEnd); 
+  const live=isLiveNow(now, startTime, endTime);
+
+  if (isLiveNow(now, startTime, endTime)) {
+    console.log("Current time is between start and end time");
+  } else {
+    console.log("Current time is not between start and end time");
+  }
+  
 
 
   return (
@@ -37,6 +59,15 @@ const CustomCard = ({ title, schedule, webex, eclass, semester, isFavorite, togg
       </div>
       <div className="content" style={{ flex: '1', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
         <h6 style={{ fontWeight: "bold", fontSize: 18 }}>Εξάμηνο: {semester}ο</h6>
+        <div className="live-section" style={{ textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <span style={{ display: 'flex' }}>
+            {live ? <Circle style={{ color: 'red', fontSize: '1em' }}/> : <Circle style={{ fontSize: '1em', color: 'gray' }}/>}
+          </span>
+          <p style={{ marginLeft: "8px", color: live ? 'red' : 'gray' }}>
+            {live ? 'Ζωντανά τώρα' : 'Eκτός σύνδεσης'}
+          </p>
+        </div>
+
         <div className="schedule-section">
           <h4 style={{ textDecoration: "underline", textAlign: "center" }}>Πρόγραμμα:</h4>
           <ul style={{ listStyle: "none", padding: "0", textAlign: "center" }}>
