8aea807f32f23d4051823e28eb515d499887462e
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-03-06 13:37:14 +0200

    Small fix

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p style={{display:"flex",justifyContent:"center"}}>&copy; {new Date().getFullYear()}&nbsp; <a href="https://github.com/mgiannopoulos24"> mgiannopoulos24</a>, <a href="https://github.com/kokkinos64">V. Kokkinos</a>. All rights reserved.</p>   
+      <p style={{display:"flex",justifyContent:"center"}}>&copy; {new Date().getFullYear()}&nbsp; <a href="https://github.com/mgiannopoulos24"> mgiannopoulos24</a>,&nbsp; <a href="https://github.com/kokkinos64">V. Kokkinos</a>. All rights reserved.</p>   
     </footer>
   );
 }
